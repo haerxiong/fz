15b5beb27d0fee59c4906a3b4bd9af1325d02c1c
liuwei <duguwei888@126.com>
2020-12-14 13:56:41 +0800

    增加restart.sh

--- restart.sh
+++ restart.sh
@@ -1,2 +1,2 @@
-jps|grep fz.jar|awk {'print $1'}|xargs -I {} kill -9 {}
+jps|grep fz.jar|awk {'print $1'}|xargs -I {} kill {}
 sudo nohup java -jar /var/lib/jenkins/workspace/fz/target/fz.jar --server.port=8092 &
